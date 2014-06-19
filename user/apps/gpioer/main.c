e9f8f753af3f5a69c28535f045cd918da24df829
gapry <gapryun@gmail.com>
2014-06-20 03:58:13 +0800

    just trace

--- user/apps/gpioer/main.c
+++ user/apps/gpioer/main.c
@@ -12,7 +12,10 @@
 
 #define STACK_SIZE 256
 
-enum { GPIOER_THREAD, BUTTON_MONITOR_THREAD };
+enum {
+	GPIOER_THREAD, 
+	BUTTON_MONITOR_THREAD 
+};
 
 static L4_ThreadId_t threads[2] __USER_DATA;
 
@@ -29,7 +32,7 @@ static L4_Word_t free_mem __USER_DATA;
 
 static uint8_t board_leds[BOARD_LED_NUM] __USER_DATA;
 
-static inline void __USER_TEXT led_init(void)
+static inline void __USER_TEXT led_init (void)
 {
 
 	board_leds[0] = BOARD_LED_PIN1;
@@ -37,38 +40,42 @@ static inline void __USER_TEXT led_init(void)
 	board_leds[2] = BOARD_LED_PIN3;
 	board_leds[3] = BOARD_LED_PIN4;
 
-	for (int i = 0; i < BOARD_LED_NUM; ++i)
-	{
-		gpio_config_output(BOARD_LED_PORT,
+	for (int i = 0; i < BOARD_LED_NUM; ++i) {
+		gpio_config_output (
+			BOARD_LED_PORT,
 			board_leds[i],
 			GPIO_PUPDR_UP,
-			GPIO_OSPEEDR_50M);
+			GPIO_OSPEEDR_50M
+		);
 	}
 }
 
-static inline void __USER_TEXT leds_onoff(bool on)
+static inline void __USER_TEXT leds_onoff (bool on)
 {
-	for (int i = 0; i < BOARD_LED_NUM; ++i)
-	{
-		if (on)
-            gpio_out_high(BOARD_LED_PORT, board_leds[i]);
-        else
-            gpio_out_low(BOARD_LED_PORT, board_leds[i]);
+	for (int i = 0; i < BOARD_LED_NUM; ++i) {
+		if (on) {
+            gpio_out_high (BOARD_LED_PORT, board_leds[i]);
+		} else {
+            gpio_out_low (BOARD_LED_PORT, board_leds[i]);
+		}
 	}
 }
 
 
-void __USER_TEXT gpioer_thread(void)
+void __USER_TEXT gpioer_thread (void)
 {
 	bool flag = true;
 
-	printf("gpioer thread: built-in leds blinking\n");
-    led_init();
-    while (1)
-    {
-            leds_onoff(flag);
-            L4_Sleep(L4_TimePeriod(1000 * 1000));
-            flag=!flag;
+	printf ("gpioer thread: built-in leds blinking\n");
+    
+	led_init ();
+
+    while (1) {
+        leds_onoff (flag);
+		
+		L4_Sleep (L4_TimePeriod (1000 * 1000));
+
+		flag = !flag;
     }
 }
 
@@ -88,68 +95,71 @@ void __USER_TEXT gpioer_thread(void)
 
 #define BUTTON_CUSTOM_PIN BUTTON_USER_PIN
 
-void __USER_TEXT button_monitor_thread(void)
+void __USER_TEXT button_monitor_thread (void)
 {
     int count = 1;
 
-    gpio_config_input(GPIOA, BUTTON_CUSTOM_PIN, GPIO_PUPDR_DOWN);
-	printf("thread: built-in user button detection\n");
-    while (1)
-    {
-            uint8_t state = gpio_input_bit(GPIOA, BUTTON_CUSTOM_PIN);
-            if (state != 0) {
-                printf("button %s %d times\n", state == 0 ? "open" : "pushed", count);
-                count++;
-            }
-            L4_Sleep(L4_TimePeriod(1000 * 200));
+    gpio_config_input (GPIOA, BUTTON_CUSTOM_PIN, GPIO_PUPDR_DOWN);
+	
+	printf ("thread: built-in user button detection\n");
+    while (1) {
+        uint8_t state = gpio_input_bit (GPIOA, BUTTON_CUSTOM_PIN);
+
+        if (state != 0) {
+            printf ("button %s %d times\n", state == 0 ? "open" : "pushed", count++);
+        }
+
+		L4_Sleep (L4_TimePeriod (1000 * 200));
     }
 }
 
-static void __USER_TEXT start_thread(L4_ThreadId_t t, L4_Word_t ip,
-									 L4_Word_t sp, L4_Word_t stack_size)
+static void __USER_TEXT start_thread (L4_ThreadId_t t, 
+									  L4_Word_t ip,
+									  L4_Word_t sp, 
+									  L4_Word_t stack_size)
 {
 	L4_Msg_t msg;
 
-	L4_MsgClear(&msg);
-	L4_MsgAppendWord(&msg, ip);
-	L4_MsgAppendWord(&msg, sp);
-	L4_MsgAppendWord(&msg, stack_size);
-	L4_MsgLoad(&msg);
+	L4_MsgClear (&msg);
+	L4_MsgAppendWord (&msg, ip);
+	L4_MsgAppendWord (&msg, sp);
+	L4_MsgAppendWord (&msg, stack_size);
+	L4_MsgLoad (&msg);
 
-	L4_Send(t);
+	L4_Send (t);
 }
 
-static L4_ThreadId_t __USER_TEXT create_thread(user_struct *user, void (*func)(void))
+static L4_ThreadId_t __USER_TEXT create_thread (user_struct *user, void (*func)(void))
 {
-	L4_ThreadId_t myself = L4_MyGlobalId();
+	L4_ThreadId_t myself = L4_MyGlobalId ();
 	L4_ThreadId_t child;
 
 	child.raw = myself.raw + (++last_thread << 14);
 
-	L4_ThreadControl(child, myself, L4_nilthread, myself, (void *) free_mem);
+	L4_ThreadControl (child, myself, L4_nilthread, myself, (void *) free_mem);
 	free_mem += UTCB_SIZE + STACK_SIZE;
 
-	start_thread(child, (L4_Word_t)func, free_mem, STACK_SIZE);
+	start_thread (child, (L4_Word_t)func, free_mem, STACK_SIZE);
 
 	return child;
 }
 
-static void __USER_TEXT main(user_struct *user)
+static void __USER_TEXT main (user_struct *user)
 {
 	free_mem = user->fpages[0].base;
 
-	threads[GPIOER_THREAD] = create_thread(user, gpioer_thread);
-	threads[BUTTON_MONITOR_THREAD] = create_thread(user, button_monitor_thread);
+	threads[GPIOER_THREAD] = create_thread (user, gpioer_thread);
+	threads[BUTTON_MONITOR_THREAD] = create_thread (user, button_monitor_thread);
 }
 
 #define DEV_SIZE 0x3c00
 #define AHB1_1DEV 0x40020000
 
-DECLARE_USER(
+DECLARE_USER (
 	0,
 	gpioer,
 	main,
-	DECLARE_FPAGE(0x0, 2 * UTCB_SIZE + 2 * STACK_SIZE)
+	DECLARE_FPAGE (0x0, 2 * UTCB_SIZE + 2 * STACK_SIZE)
     /* map thread with AHB DEVICE for gpio accessing */
-	DECLARE_FPAGE(AHB1_1DEV, DEV_SIZE)
+	DECLARE_FPAGE (AHB1_1DEV, DEV_SIZE)
 );
