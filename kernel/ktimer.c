4a75dc64a19906df53a962dd603cd07120dee013
gapry <gapryun@gmail.com>
2014-07-11 03:43:20 +0800

    Use DWT to measure the time

--- kernel/ktimer.c
+++ kernel/ktimer.c
@@ -88,66 +88,81 @@ IRQ_HANDLER(ktimer_handler, __ktimer_handler);
 #ifdef CONFIG_KDB
 
 #define MAX_BYTES 5440
-#define SYSTICKS_REG 0xE000E018
 
-char test_src[4] = {'A', 'B', 'C', 'D'};
-char test_dest[MAX_BYTES];
+#define CYCLE_COUNT_REGADDR							0xE0001004 
+#define CONTROL_REGADDR								0xE0001000
+#define DEBUG_EXCEPTION_MONITOR_CONTROL_REGADDR		0xE000EDFC
+ 
+static uint64_t *DWT_CYCCNT 	= (uint64_t *) CYCLE_COUNT_REGADDR; 
+static uint64_t *DWT_CONTROL 	= (uint64_t *) CONTROL_REGADDR; 
+static uint64_t *SCB_DEMCR 		= (uint64_t *) DEBUG_EXCEPTION_MONITOR_CONTROL_REGADDR;
 
-uint32_t measure_consumption (uint32_t start, uint32_t end) 
+static int cnt_enable = 0; 
+
+void dwt_cfg (void) 
 {
-	return (start < end) ? (end - start) : (start - end);	
-}
+	*SCB_DEMCR 		= *SCB_DEMCR 	| 0x01000000;
+    *DWT_CONTROL 	= *DWT_CONTROL 	| 1 ;
 
-uint32_t *get_systicks (void)
-{
-	return (uint32_t *) SYSTICKS_REG;
-}
-
-void testbench_memcpy (int len)
-{
-	uint32_t measure_start = 0;
-	uint32_t measure_end = 0;
-	
-	dbg_printf (DL_KDB, "The consumption of memcpy, unit: [ticks]\n");
-	dbg_printf (DL_KDB, "----------------------------------------\n");
-
-	for (int i = 1; i <= (MAX_BYTES / len); ++i) {
-		measure_start = *get_systicks ();		
-
-		for (int j = len; j <= i * len; j += len) {
-			memcpy (test_dest + j, test_src, len);
-		}
-
-		measure_end = *get_systicks ();
-		dbg_printf (DL_KDB, "[%d]: %ld\n", i, measure_consumption (measure_start, measure_end));
+	if (!cnt_enable) {
+    	cnt_enable 	= 1;	
+		*DWT_CYCCNT = 0; 
 	}
+}
 
-	dbg_printf (DL_KDB, "----------------------------------------\n");
+uint64_t *fetch_cyccnt (void)
+{
+	dwt_cfg ();
+
+	return DWT_CYCCNT;
+}
+
+void measure_alignment_unit (void)
+{
+	uint64_t start 	= 0;
+	uint64_t end 	= 0;	
+	
+	start = *fetch_cyccnt ();	
+	
+	for (int i = 0; i < 4; ++i) {
+	} 
+	
+	end = *fetch_cyccnt ();
+
+	dbg_printf (DL_KDB, "The consumption of alignment unit: %ld\n", end - start);	
+}
+
+void measure_maximum_char_blocks (void)
+{
+	uint64_t start	= 0;
+	uint64_t end 	= 0;
+
+	start = *fetch_cyccnt ();
+	
+	for (int i = 0; i < MAX_BYTES; ++i) {
+	}	
+
+	end = *fetch_cyccnt ();
+
+	dbg_printf (DL_KDB, "The consumption of maximum of char blocks: %ld\n", end - start);
 }
 
 void kdb_show_ktimer(void)
 {
-	testbench_memcpy (3);
-	testbench_memcpy (4);
-
-	//testbench_memcpy_alignment ();
-
-	//uint32_t *foo = (uint32_t *) 0xE000E018;
-	//uint32_t start = *get_systicks ();
+	dbg_printf (DL_KDB, "-----------------------------\n");
+	dbg_printf (DL_KDB, "benchmark: \n");
 	
-	//dbg_printf(DL_KDB, "foo is %ld\n", start);
+	measure_alignment_unit ();	
+	measure_maximum_char_blocks ();
 
-	dbg_printf(DL_KDB, "Now is %ld\n", ktimer_now);
+	dbg_printf (DL_KDB, "-----------------------------\n");
+
+	dbg_printf (DL_KDB, "Now is %ld\n", ktimer_now);
 
 	if (ktimer_enabled) {
 		dbg_printf(DL_KDB,
 		           "Ktimer T=%d D=%d\n", ktimer_time, ktimer_delta);
 	}
-	/*
-	uint32_t end = *get_systicks ();
-	dbg_printf(DL_KDB, "foo is %ld\n", end);
-	dbg_printf(DL_KDB, "foo is %ld\n", measure_consumption (start, end));
-	*/
 }
 
 #if defined(CONFIG_KTIMER_TICKLESS) && defined(CONFIG_KTIMER_TICKLESS_VERIFY)
