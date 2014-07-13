535d8a150e8969e130ba0c97b4b0e8e58b27f701
gapry <gapryun@gmail.com>
2014-07-13 23:13:32 +0800

    Benchmark: define the prototypes of benchmark functions.

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -18,6 +18,11 @@ struct benchmark_t {
     benchmark_function_t function;
 };
 
+void dwt_stream_copy_access_unalignment (void); 
+void dwt_stream_copy_access_alignment (void); 
+void systicks_stream_copy_access_unalignment (void); 
+void systicks_stream_copy_access_alignment (void); 
+
 static uint64_t *DWT_CYCCNT     = (uint64_t *) CYCLE_COUNT_REGADDR; 
 static uint64_t *DWT_CONTROL    = (uint64_t *) CONTROL_REGADDR; 
 static uint64_t *SCB_DEMCR      = (uint64_t *) DEBUG_EXCEPTION_MONITOR_CONTROL_REGADDR;
