1859036025ec610eef0a5181b620dd4e173301dc
gapry <gapryun@gmail.com>
2014-08-22 12:23:36 +0800

    Release: version-3

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -6,7 +6,7 @@
 #include <benchmark/benchmark.h>
 
 #define MAX_BYTES		0x4000
-#define RESULT_BYTES	0x1000
+#define RESULT_BYTES	13
 
 __attribute__ ((section (".src_pool"))) char src[MAX_BYTES]  = { 'T' };
 __attribute__ ((section (".dest_pool"))) char dest[MAX_BYTES] = { 'D' };
@@ -22,14 +22,14 @@ void profiler_main (void)
 {
 	uint32_t start = 0;
 	uint32_t end = 0;
-	uint32_t offset = 4;
+	uint64_t offset = 4;
 	int n_iteration = 1000;
 
 	*SYSTICK_CTL = 0x00000007;
 	init_systick (CONFIG_KTIMER_HEARTBEAT, 0);
 	
 	for (uint32_t j = 0; j < RESULT_BYTES; ++j) {
-		offset = offset + 4;
+		offset = 4 << j;
 		
 		start = *SYSTICK_VAL;
 
